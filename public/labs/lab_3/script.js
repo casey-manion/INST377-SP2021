583d94e5c48e3e8d34c385e86799c6a8682b3a98
casey-manion <78034730+casey-manion@users.noreply.github.com>
2021-02-13 00:01:51 -0500

    UPDATE: Lab 3 - adjusted HTML/CSS, JS carousel
    - small adjustments to HTML/CSS
    - implemented possible JS carousel function

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,54 @@
 /* Put your javascript in here */
+function carousel_slider() {
+    /* Buttons */
+    let nextBtn = document.querySelector('#next');
+    let prevBtn = document.querySelector('#prev');
+
+    /* indexing */
+    let x = 0;
+    let y = 2;
+
+    /* Build images array */
+    let images = document.querySelectorAll('.item');
+    const imgArr = Array.from(images);
+
+    prevBtn.onclick = (event) => {
+        event.preventDefault();
+
+        images[x].classlist.remove("active");
+
+        if (y <= 0) {
+            y = images.length - 1;
+            x--;
+        }
+        else if (x <= 0) {
+            x = images.length - 1;
+            y--;
+        }
+        images[y].classList.add("active");
+    };
+
+    nextBtn.onclick = (event) => {
+        event.preventDefault();
+
+        images[x].classList.remove("active");
+
+        if (y >= images.length) {
+            y = 0;
+            x++;
+        }
+        else if (x >= images.length) {
+            x = 0;
+            y++
+        }
+        else {
+            x++;
+            y++;
+        }
+
+        images[y].classList.add("active");
+    };
+
+}
+
+window.onload = carousel_slider;
