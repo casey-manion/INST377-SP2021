754bbfe3cb9d1315e55e418ead2b3862eb46a2d3
casey-manion <78034730+casey-manion@users.noreply.github.com>
2021-03-10 15:53:51 -0500

    UPDATE: Lab 5 - setup index and js
    - index needs map still
    - js typeahead still needs to be slightly altered from A1 code

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -6,11 +6,63 @@ function mapInit() {
 async function dataHandler(mapObjectFromFunction) {
   // use your assignment 1 data handling code here
   // and target mapObjectFromFunction to attach markers
+
+  // API PG County Food Inspection data
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const searchInput = document.querySelector('#input');
+  const suggestions = document.querySelector('.suggestions');
+  
+  // fetch request
+  const request = await fetch(endpoint)
+  // empty array for data
+  const zipcodes = await request.json();
+  
+  // check for matches using input box compared to zipcodes array
+  function findMatches(wordToMatch, zipcodes) {
+      return zipcodes.filter(place => {
+      const regex = new RegExp(wordToMatch, "gi");
+      return place.zip.match(regex) || place.city.match(regex)
+      });
+  }
+  
+  // display matches found
+  function displayMatches(event) {
+      const matchArray = findMatches(event.target.value, zipcodes);
+      const html = matchArray.map(place => {
+          const regex = new RegExp(this.value, "gi");
+          const cityName = place.city;
+          const zipCode = place.zip;
+          const addressLine1 = place.address_line_1;
+          const addressLine2 = place.address_line_2;
+          const restaurantName = place.name;
+
+          return `
+              <div class="box is-small">
+                  <li>
+                      <div class="name">${restaurantName}</div>
+                      <address class="address">${addressLine1}, ${addressLine2}</address>
+                      <address class="address">${cityName}, ${zipCode}</address>
+                  </li>
+              </div>
+              `;
+      }).join('');
+      suggestions.innerHTML = html;
+  }
+  
+  // event listeners
+  searchInput.addEventListener('submit', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => {
+      displayMatches(evt)
+  });
+
+  
 }
 
 async function windowActions() {
   const map = mapInit();
   await dataHandler(map);
+
+  
 }
 
 window.onload = windowActions;
