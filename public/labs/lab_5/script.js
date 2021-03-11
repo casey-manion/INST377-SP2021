b656f2752451c5cf806e39f7c5d5e7ffa49e395d
casey-manion <78034730+casey-manion@users.noreply.github.com>
2021-03-11 00:28:13 -0500

    UPDATE: Lab 5 - slight adjusments
    - index bulma updates
    - js tweak event listener (needs work)
    - js cleaned up code

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,47 +1,54 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
+  map = 'DELETE ME'
   return map;
 }
 
 async function dataHandler(mapObjectFromFunction) {
   // use your assignment 1 data handling code here
   // and target mapObjectFromFunction to attach markers
-
+  console.log('into dataHandler now');
   // API PG County Food Inspection data
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
   const searchInput = document.querySelector('#input');
+  const form = document.querySelector('#form');
+  const btnSearch = document.querySelector('.button');
   const suggestions = document.querySelector('.suggestions');
   
   // fetch request
   const request = await fetch(endpoint)
+  
+  if (request.ok) {
+    console.log('endpoint fetched');
+  }
+  else {
+    alert("HTTP-Error: " + request.status);
+  }
   // empty array for data
   const zipcodes = await request.json();
   
   // check for matches using input box compared to zipcodes array
-  function findMatches(wordToMatch, zipcodes) {
+  function findMatches(targetZip, zipcodes) {
       return zipcodes.filter(place => {
-      const regex = new RegExp(wordToMatch, "gi");
-      return place.zip.match(regex) || place.city.match(regex)
+        const regex = new RegExp(targetZip, "gi");
+        return place.zip.match(regex)
       });
   }
   
   // display matches found
   function displayMatches(event) {
+      console.log('entered displayMatches');
       const matchArray = findMatches(event.target.value, zipcodes);
       const html = matchArray.map(place => {
           const regex = new RegExp(this.value, "gi");
-          const cityName = place.city;
-          const zipCode = place.zip;
-          const addressLine1 = place.address_line_1;
-          const addressLine2 = place.address_line_2;
-          const restaurantName = place.name;
 
           return `
-              <div class="box is-small">
+              <div class="box">
                   <li>
-                      <div class="name">${restaurantName}</div>
-                      <address class="address">${addressLine1}, ${addressLine2}</address>
-                      <address class="address">${cityName}, ${zipCode}</address>
+                      <div class="name">${place.name}</div>
+                      <address class="address">${place.address_line_1}, ${place.address_line_2}</address>
+                      <address class="address">${place.city}, ${place.zip}</address>
                   </li>
               </div>
               `;
@@ -50,10 +57,13 @@ async function dataHandler(mapObjectFromFunction) {
   }
   
   // event listeners
-  searchInput.addEventListener('submit', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => {
-      displayMatches(evt)
-  });
+  form.addEventListener('input', displayMatches);
+  // form.addEventListener('submit', (evt) => {
+  //   displayMatches(evt)
+  // });
+  // btnSearch.addEventListener('input', (evt) => {
+  //     displayMatches(evt)
+  // });
 
   
 }
